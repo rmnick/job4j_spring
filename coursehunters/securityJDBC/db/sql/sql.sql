8efaa084cdfb1f9049f094edf9436d7bfaa6cad3
rmnick <r.m.nick@yandex.ru>
2019-12-03 18:39:28 +0500

    some sql fix

--- coursehunters/securityJDBC/db/sql/sql.sql
+++ coursehunters/securityJDBC/db/sql/sql.sql
@@ -1,10 +1,12 @@
 create table users (username varchar(50) primary key
-					, password varchar(50)
-					, enabled smallint not null);
+					, password varchar(68)
+					, enabled boolean not null);
 
 insert into users values ('john', '{noop}test123', 1)
                           ,('mary', '{noop}test123', 1)
 						  , ('nick', '{noop}test123', 1);
+
+-- or '{bcrypt} ...password...'
 
 create table if not exists authorities (username varchar(50) not null references users (username)
 										, authority varchar(50) not null
@@ -15,3 +17,4 @@ insert into authorities values ('john', 'ROLE_EMPLOYEE')
 , ('mary', 'ROLE_MANAGER')
 , ('nick', 'ROLE_EMPLOYEE')
 , ('nick', 'ROLE_ADMIN');
+
