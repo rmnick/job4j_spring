02c070376972101f17a0650824796d74a27d5b62
rmnick <r.m.nick@yandex.ru>
2019-12-05 22:55:09 +0500

    start RestApi module

---
+++ coursehunters/rest/src/main/java/org/chubaka/config/MyRestAppDispatcherServletInitializer.java
@@ -0,0 +1,20 @@
+package org.chubaka.config;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class MyRestAppDispatcherServletInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return null;
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[] {RestAppConfig.class};
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[] {"/"};
+    }
+}
---
+++ coursehunters/rest/src/main/webapp/index.jsp
@@ -0,0 +1,18 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: User
+  Date: 05.12.2019
+  Time: 22:46
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>demo</title>
+</head>
+<body>
+<h2>demo start page</h2>
+<hr>
+<a href="${pageContext.request.contextPath}/test/hello">hello</a>
+</body>
+</html>
